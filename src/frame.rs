f911310d50f1d28b1849f96e23eb7ae209071e58
brook-bang <1455047806@qq.com>
2025-03-10 20:47:22 +0800

    add

--- src/frame.rs
+++ src/frame.rs
@@ -4,3 +4,73 @@ use std::fmt;
 use std::io::Cursor;
 use std::num::TryFromIntError;
 use std::string::FromUtf8Error;
+
+#[derive(Clone, Debug)]
+pub enum Frame {
+    Simple(String),
+    Error(String),
+    Integer(u64),
+    Bulk(Bytes),
+    Null,
+    Array(Vec<Frame>),
+}
+
+#[derive(Debug)]
+pub enum Error {
+    Incomplete,
+    Other(crate::Error),
+}
+
+impl Frame {
+    pub(crate) fn array() -> Frame {
+        Frame::Array(vec![])
+    }
+
+    pub(crate) fn push_bulk(&mut self, bytes: Bytes) {
+        match self {
+            Frame::Array(vec) => {
+                vec.push(Frame::Bulk(bytes));
+            }
+            _ => panic!("not an array frame"),
+        }
+    }
+
+    pub(crate) fn push_int(&mut self, value: u64) {
+        match self {
+            Frame::Array(vec) => {
+                vec.push(Frame::Integer(value));
+            }
+            _ => panic!("not an array frame"),   
+        }
+    }
+
+    pub fn check(src: &mut Cursor<&[u8]>) -> Result<(),Error> {
+        match get_u8(src)? {
+            b'+' => {
+                
+            }
+            
+        }
+    }
+}
+
+fn get_u8(src: &mut Cursor<&[u8]>) -> Result<u8,Error> {
+    if !src.has_remaining() {
+        return Err(Error::Incomplete);
+    }
+    Ok(src.get_u8())
+}
+
+fn get_line<'a>(src: &mut Cursor<&'a [u8]>) -> Result<&'a [u8],Error> {
+    let start = src.position() as usize;
+    let end = src.get_ref().len() - 1;
+    for i in start..end {
+        if src.get_ref()[i] == b'\r' &&src.get_ref()[i+1] == b'\n' {
+            
+        }
+        
+    }
+}
+
+
+
