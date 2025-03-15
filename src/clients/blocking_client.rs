771e4c3a13761ca3e4ce7ef40867db1e1239879d
brook-bang <1455047806@qq.com>
2025-03-15 22:26:53 +0800

    add

--- src/clients/blocking_client.rs
+++ src/clients/blocking_client.rs
@@ -3,4 +3,4 @@ use std::time::Duration;
 use tokio::net::ToSocketAddrs;
 use tokio::runtime::Runtime;
 
-pub use crate::clients::Message;
+pub use crate::clients::Message; 
--- src/clients/client.rs
+++ src/clients/client.rs
@@ -1,10 +1,124 @@
 use crate::cmd::{Get, Ping, Publish, Set, Subscribe, Unsubscribe};
-use crate::{Connection, Frame};
+use crate::{connection, frame, Connection, Frame};
 
 use async_stream::try_stream;
 use bytes::Bytes;
 use std::io::{Error, ErrorKind};
+use std::sync::mpsc::channel;
 use std::time::Duration;
 use tokio::net::{TcpStream, ToSocketAddrs};
 use tokio_stream::Stream;
 use tracing::{debug, instrument};
+
+pub struct Client {
+    connection: Connection,
+}
+
+pub struct Subscriber {
+    client: Client,
+    subscribed_channels: Vec<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Message {
+    pub channel: String,
+    pub content: Bytes,
+}
+
+impl Client {
+    pub async fn connect<T: ToSocketAddrs>(addr: T) -> crate::Result<Client> {
+        let socket = TcpStream::connect(addr).await?;
+        let connection = Connection::new(socket);
+        Ok(Client { connection })
+    }
+
+    #[instrument(skip(self))]
+    pub async fn ping(&mut self, msg: Option<Bytes>) -> crate::Result<Bytes> {
+        let frame = Ping::new(msg).into_frame();
+        debug!(request = ?frame);
+        self.connection.write_frame(&frame).await?;
+        match self.read_response().await? {
+            Frame::Simple(value) => Ok(value.into()),
+            Frame::Bulk(value) => Ok(value),
+            frame => Err(frame.to_error()),
+        }
+    }
+
+    //#[instrument(skip(self))]
+    pub async fn get(&mut self, key: &str) -> crate::Result<Option<Bytes>> {
+        let frame = Get::new(key).into_frame();
+        debug!(request = ?frame);
+        self.connection.write_frame(&frame).await?;
+        match self.read_response().await? {
+            Frame::Simple(value) => Ok(Some(value.into())),
+            Frame::Bulk(value) => Ok(Some(value)),
+            Frame::Null => Ok(None),
+            frame => Err(frame.to_error()),
+        }
+    }
+
+    pub async fn set(&mut self,key: &str,value: Bytes) -> crate::Result<()> {
+        self.set_cmd(Set::new(key, value, None)).await
+    }
+
+    #[instrument(skip(self))]
+    pub async fn set_expires(
+        &mut self,
+        key: &str,
+        value: Bytes,
+        expiration: Duration,
+    ) -> crate::Result<()> {
+        self.set_cmd(Set::new(key, value, Some(expiration))).await
+    }
+    
+    
+    async fn set_cmd(&mut self,cmd: Set) -> crate::Result<()> {
+        let frame = cmd.into_frame();
+        debug!(request = ?frame);
+        self.connection.write_frame(&frame).await?;
+        match self.read_response().await? {
+            Frame::Simple(response) if response == "Ok" => Ok(()),
+            frame => Err(frame.to_error()),
+        }
+    }
+
+    #[instrument(skip(self))]
+    pub async fn publish(&mut self,channel: &str,message: Bytes) -> crate::Result<u64> {
+        let frame = Publish::new(channel, message).into_frame();
+        debug!(request = ?frame);
+        self.connection.write_frame(&frame).await?;
+        match self.read_response().await? {
+            Frame::Integer(response) => Ok(response),
+            frame => Err(frame.to_error()),
+        }
+    }
+
+    pub async fn subscribe(mut self,channel: Vec<String>) -> crate::Result<Subscriber> {
+        self.subscribe(channel)
+    }
+
+    async fn subscribe_cmd(&mut self,channels: &[String]) -> crate::Result<()> {
+        let frame = Subscribe::new(channels.to_vec()).into_frame();
+        debug!(request = ?frame);
+        self.connection.write_frame(&frame).await?;
+        for channel in channels {
+            let response = self.read_response().await?;
+            
+        }
+    }
+
+
+
+    async fn read_response(&mut self) -> crate::Result<Frame> {
+        let response = self.connection.read_frame().await?;
+        debug!(?response);
+        match response {
+            Some(Frame::Error(msg)) => Err(msg.into()),
+            Some(frame) => Ok(frame),
+            None => {
+                let err = Error::new(ErrorKind::ConnectionReset, "服务器重置连接");
+                Err(err.into())
+            }
+        }
+    }
+}
