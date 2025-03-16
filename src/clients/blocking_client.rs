1e495e7c5478317185c694ba1f3607fd31e07f65
brook-bang <1455047806@qq.com>
2025-03-16 17:16:43 +0800

    finish buffer_client

--- src/clients/blocking_client.rs
+++ src/clients/blocking_client.rs
@@ -4,3 +4,12 @@ use tokio::net::ToSocketAddrs;
 use tokio::runtime::Runtime;
 
 pub use crate::clients::Message; 
+
+pub struct BlockingClient {
+    inner: crate::clients::Client,
+    rt: Runtime,
+}
+
+pub struct BlockingSubscriber {
+    
+}
